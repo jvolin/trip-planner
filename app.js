89f58e8b66d118ef9b1543caa8786e9b66b5a58d
Jane Volin <janevolin@yahoo.com>
2017-08-11 13:53:30 -0400

    defining tables

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const nunjucks = require('nunjucks');
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(function (err, req, res, next) {
+  res.render('error');
+});
+
+const PORT = 3000;
+
+db.sync()
+.then(() => {
+  console.log('db synced!');
+  app.listen(PORT, function () {
+    console.log(`server listening on port ${PORT}`);
+  });
+});
---
+++ models/index.js
@@ -0,0 +1,74 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432');
+
+module.exports = db;
+
+var Place = db.define('place', {
+  address: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+  city:{
+    type: Sequelize.string,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+  location:{ //(lat, lon float array)
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    defaultValue: []
+  }
+});
+
+var Hotel = db.define('hotel', {
+  name: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+  num_stars: {
+    type: Sequelize.string,//(float from 1-5)
+    allowNull: false
+  },
+  amenities: {
+    type: Sequelize.string, //(string of comma delimited items)
+    allowNull: false
+  },
+})
+
+var Activity = db.define('activity', {
+  name: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+  age_range: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+})
+
+var Restaurant = db.define('restaurant', {
+  name: {
+    type: Sequelize.string,
+    allowNull: false
+  },
+  cuisine: {
+    type: Sequelize.string,  // (comma delimited string list)
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.string, //(integer from 1-5 for how many dollar signs)
+    allowNull: false
+  },
+
+})
+
+
+var Associations = db.define('associations', {
+
+})
